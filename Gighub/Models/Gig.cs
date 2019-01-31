04c9ef191b2874e590542e4b8c66a66d157b0c29
Keenan Irvin-Benning <kirvinb@outlook.com>
2019-01-31 18:21:07 -0500

    Created db classes

---
+++ Gighub/Models/Gig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gighub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public String Venue { get; set; }
+    }
+
+}
