0cc68926bc090229600c7e929742935c0bd30110
Keenan Irvin-Benning <kirvinb@gmail.com>
2019-01-31 21:01:06 -0500

    Create add a gig form

---
+++ Gighub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Gighub.Models;
+
+namespace Gighub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public object Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}
