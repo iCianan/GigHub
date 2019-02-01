72248fb83a37b0398299ef2177d2145b035344ae
Keenan Irvin-Benning <kirvinb@gmail.com>
2019-01-31 19:45:54 -0500

    Create gig model

--- Gighub/Models/Genre.cs
+++ Gighub/Models/Genre.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,9 @@ namespace Gighub.Models
     public class Genre
     {
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public String Name { get; set; }
 
     }
